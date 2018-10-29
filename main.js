0fd5829232b2448964403f09b4fa5ee3d2f4d053
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2018-10-29 14:14:53 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var arrayOne = [ 3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1 ];
+
+function arrayOperation (array, operation) {
+  var i;
+  var sum = 0;
+  var min =
+  if ( operation ==='sum' ) {
+    for ( i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    console.log( "Summa " + sum );
+  }
+
+  if ( operation === 'min' ) {
+    // min =
+  }
+
+}
+
+arrayOperation(arrayOne, 'sum');
